3af902426f78748291a380465d0dcfd26e4e7c6a
Zeel-Ramani <zeelrramani@gmail.com>
2026-02-15 19:35:09 +0530

    exam

--- E-Commerce-Exam/config/db.js
+++ E-Commerce-Exam/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const dbconnection = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://ZeelRamani:zeel1234@zeelramani.2xisn6j.mongodb.net"
+      "mongodb+srv://ZeelRamani:zeel1234@zeelramani.2xisn6j.mongodb.net/E-Commerce-Exam"
     );
 
     console.log("Database connected successfully");
